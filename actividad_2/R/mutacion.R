f5e7ce10d00011afc94dc5bde47370583505d06e
Pablo Hidalgo <pablohigar@gmail.com>
2018-01-11 18:57:01 +0100

    Desarrollo de recombinacion

--- actividad_2/R/mutacion.R
+++ actividad_2/R/mutacion.R
@@ -32,12 +32,57 @@ mutacion_es_no_correladas <- function(individuo,
 
 mutacion_es_no_correladas(ind, 1, 0, 0.3)
 
-recombinacion <- function(ind, tipo = "discreta", lambda, mu){
-  if (! tipo %in% c("discreta", "intermedia")) 
+ind1 <- list(x = 1:5,
+            sigma = 0.5)
+
+ind2 <- list(x = 2:6,
+             sigma = 0.75)
+
+ind3 <- list(x = 3:7,
+             sigma = 0.05)
+
+poblacion <- list(ind1, ind2, ind3)
+
+recombinacion <- function(poblacion, 
+                          tipo.x = "discreta",
+                          tipo.param = "intermedia", 
+                          global = TRUE,
+                          lambda, mu){
+  
+  if (!all(c(tipo.x, tipo.param) %in% c("discreta", "intermedia"))) 
     stop("El tipo de recombinación debe ser discreta o intermedia.")
-  if (tipo == "discreta"){
-    
+  
+  dim.x <- length(poblacion[[1]]$x)
+  poblacion <- do.call(rbind, lapply(poblacion, unlist))
+  dim.poblacion <- nrow(poblacion)
+  dim.ind <- ncol(poblacion)
+   
+  
+  if (global){
+    muestra_tam2 <- function(dim.poblacion) sample(1:dim.poblacion, 2)
+    padres <- replicate(dim.ind, muestra_tam2(dim.poblacion), simplify = F)
   } else {
-    
+    padres <- list(muestra_tam2(dim.poblacion))
+    padres <- rep(padres, dim.ind)
   }
+  
+  hijo <- numeric(dim.ind)
+  for (n in 1:dim.x){
+    if (tipo.x == "discreta"){
+      hijo[n] <- sample(poblacion[padres[[n]], n], 1)
+    } else {
+      hijo[n] <- mean(poblacion[padres[[n]], n], 1)
+    }
+  }
+  
+  for (n in (dim.x + 1):dim.ind){
+    if (tipo.param == "discreta"){
+      hijo[n] <- sample(poblacion[padres[[n]], n], 1)
+    } else {
+      hijo[n] <- mean(poblacion[padres[[n]], n], 1)
+    }
+  }
+  return(list(x = hijo[1:dim.x],
+              sigma = hijo[(dim.x + 1):dim.ind]))
 }
+
