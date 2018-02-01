0b5968770d3a50c0c64834523ea69b51db91cf10
Pablo Hidalgo <pablohigar@gmail.com>
2018-02-01 19:22:33 +0100

    Comienzo de discusión de resultados de la función de Powell

--- actividad_2/funciones_a_optimizar.R
+++ actividad_2/funciones_a_optimizar.R
@@ -17,50 +17,76 @@ xin_she_yang_4 <- function(x){
 xin_she_yang_4(1:2)
 
 
-
-# Gráficos ----------------------------------------------------------------
-library(plotly)
-library(magrittr)
-library(plot3D)
-
-set.seed(123)
-n <- 50
-x <- seq( -1, 1, length.out = n)
-y <- seq( -1, 1, length.out = n)
-a <- expand.grid(x, y)
-z <- apply(a, 1, suma_powell)
-
-d <- data.frame(a, z)
-ggplot(d, aes(Var1, Var2, z = -z)) + geom_raster(aes(fill = -z)) + 
-  geom_contour(color = "white")
-
-ggplot(d, aes(Var1, Var2, z = z)) + geom_contour(binwidth = 0.01) + 
-  coord_equal()
-ggplot(d, aes(Var1, z)) + geom_point(aes(color = -z))
-
-v <- ggplot(faithfuld, aes(waiting, eruptions, z = density))
-v + geom_contour(binwidth = 0.1)
-
-image(x, y, z)
-x <- matrix(x, nrow = 10, ncol = 10, byrow = TRUE)
-y <- matrix(y, nrow = 10, ncol = 10, byrow = TRUE)
-z <- matrix(z, nrow = 10, ncol = 10, byrow = TRUE)
-
-outer(x, y, "sum")
-x <- cbind(x, y)
-p <- plot_ly(x, x = ~V1, y = ~V2, z = ~y) %>% add_surface()
-
-surf3D(x = x, y = y, z = z)
-
-
-X       <- seq(0, pi, length.out = 50)
-Y       <- seq(0, 2*pi, length.out = 50)
-M       <- mesh(X, Y)
-phi     <- M$x
-theta   <- M$y
-
-# x, y and z grids
-r <- sin(4*phi)^3 + cos(2*phi)^3 + sin(6*theta)^2 + cos(6*theta)^4
-x <- r * sin(phi) * cos(theta)
-y <- r * cos(phi)
-z <- r * sin(phi) * sin(theta)
+# 
+# # Gráficos ----------------------------------------------------------------
+# library(plotly)
+# library(magrittr)
+# library(plot3D)
+# 
+# set.seed(123)
+# n <- 50
+# x <- seq( -1, 1, length.out = n)
+# y <- seq( -1, 1, length.out = n)
+# a <- expand.grid(x, y)
+# z <- apply(a, 1, suma_powell)
+# 
+# d <- data.frame(a, z)
+# ggplot(d, aes(Var1, Var2, z = z)) + geom_raster(aes(fill = z)) + 
+#   geom_contour(color = "white") + 
+#   ggthemes::theme_fivethirtyeight()
+# 
+# ggplot(d, aes(Var1, Var2, z = z)) + geom_contour(binwidth = 0.01) + 
+#   coord_equal()
+# ggplot(d, aes(Var1, z)) + geom_point(aes(color = -z))
+# 
+# v <- ggplot(faithfuld, aes(waiting, eruptions, z = density))
+# v + geom_contour(binwidth = 0.1)
+# 
+# image(x, y, z)
+# x <- matrix(x, nrow = 10, ncol = 10, byrow = TRUE)
+# y <- matrix(y, nrow = 10, ncol = 10, byrow = TRUE)
+# z <- matrix(z, nrow = 10, ncol = 10, byrow = TRUE)
+# 
+# outer(x, y, "sum")
+# x <- cbind(x, y)
+# p <- plot_ly(x, x = ~V1, y = ~V2, z = ~y) %>% add_surface()
+# 
+# surf3D(x = x, y = y, z = z)
+# 
+# 
+# X       <- seq(0, pi, length.out = 50)
+# Y       <- seq(0, 2*pi, length.out = 50)
+# M       <- mesh(X, Y)
+# phi     <- M$x
+# theta   <- M$y
+# 
+# # x, y and z grids
+# r <- sin(4*phi)^3 + cos(2*phi)^3 + sin(6*theta)^2 + cos(6*theta)^4
+# x <- r * sin(phi) * cos(theta)
+# y <- r * cos(phi)
+# z <- r * sin(phi) * sin(theta)
+# 
+# 
+# 
+# par(mar = c(2, 2, 2, 2))
+# par(mfrow = c(1, 1))
+# 
+# x <- seq(-1, 1, length.out=50)
+# y <- x
+# M <- mesh(x, y)
+# 
+# alpha <- M$x
+# beta <- M$y
+# 
+# a <- cbind(as.vector(alpha), as.vector(beta))
+# 
+# z <- apply(a, 1, suma_powell)
+# 
+# b <- matrix(z, nrow = 50, ncol = 50)
+# 
+# surf3D(x      = alpha,
+#        y      = beta,
+#        z      = b,
+#        colkey = FALSE,
+#        bty    = "b2",
+#        main   = "Función suma de Powell ")
